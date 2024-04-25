aa3bee65692504501dfc38abafe1b269d02a6e3b
Vivekviv999 <sreevivek99@gmail.com>
2024-04-25 18:11:47 +0530

    add new item and open task done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello world")
+// console.log("Hello world")
+// for static approach we use below 
+// var state = {
+//     taskList: [
+//         {
+//             imageUrl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: ""
+//         },
+//           {
+//             imageUrl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: ""
+//         },
+//           {
+//             imageUrl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: ""
+//         }
+//     ] 
+// }
+
+//dynamic approach
+ 
+var state = {
+    taskList:[]
+}
+
+// DOM Objects
+var taskContents = document.querySelector(".task_contents");
+var taskModal = document.querySelector(".task__modal__body");
+
+console.log(taskModal);
+
+
